1e0597754d5789dfd3fa376c107653480f0b5d54
unknown <markwhan11@gmail.com>
2018-01-26 00:52:26 -0500

    add onAddItem method to MainActivity

--- app/src/main/java/com/example/markwilliamhan/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/markwilliamhan/simpletodo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.markwilliamhan.simpletodo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -16,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
         // Finds the ListView in activity_main.xml and links it to lvItems variable.
         lvItems = (ListView) findViewById(R.id.lvItems);
 
@@ -36,6 +35,22 @@ public class MainActivity extends AppCompatActivity {
         // Adding the first two items to the ArrayList (These would be the first two TODOs that you add to the list)
         items.add("First Item");
         items.add("Second Item");
+    }
+
+    public void onAddItem(View v) {
+        // Finds the EditText in layout
+        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+
+        // gets the Text that is written by user in the EditText box and labels it itemText
+        String itemText = etNewItem.getText().toString();
+
+        //Adds the itemText, containing what was written by user in the EditText, and adds it to the itemsAdapter
+        itemsAdapter.add(itemText);
+
+        // Erases what was previously in the EditText
+        etNewItem.setText("");
 
     }
+
+
 }
